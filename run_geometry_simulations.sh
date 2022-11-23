0281ad63b6ca286116fc01fb4ec3eb89c00f2123
simonwa7 <william.andrew.simon@gmail.com>
2022-11-22 22:20:05 -0500

    Adding bash script to run geo sims

---
+++ run_geometry_simulations.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -J qchem-diatomic-simulations
+#SBATCH -p batch 
+#SBATCH --time=7-00:00:00
+#SBATCH -n 1
+#SBATCH -c 2
+#SBATCH --mem=36000
+module load anaconda/3
+source activate /cluster/tufts/lovelab/wsimon02/condaenv/qchem
+python3 run_openfermion_classical_calculations.py geometry
