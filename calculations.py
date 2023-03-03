491ba17eea306ae8f382038b87f1d39f15012cea
simonwa7 <william.andrew.simon@gmail.com>
2023-03-03 14:37:46 -0500

    Fixing noncontextual approximation code to remove fci calcs for 20 qubits and above

--- calculations.py
+++ calculations.py
@@ -1,13 +1,12 @@
 from time import process_time
 from openfermionpsi4 import run_psi4
 from functools import partial
-from symmer.symplectic import PauliwordOp
+from symmer.symplectic import PauliwordOp, QuantumState
 from symmer.utils import exact_gs_energy
-from symmer.projection import QubitTapering
-from symmer.projection import ContextualSubspace
+from symmer.projection import QubitTapering, ContextualSubspace
 import numpy as np
 from openfermion.transforms import get_fermion_operator, jordan_wigner
-from openfermionpsi4 import run_psi4
+from openfermion import count_qubits
 
 
 def run_fci(molecule):
@@ -119,33 +118,33 @@ def run_nc(molecule, strategy="SingleSweep_magnitude"):
     """TODO: Not Yet Ready"""
     start = process_time()
     nc_data = {}
-    # 1. Generate Molecular Hamiltonian
-    qubit_hamiltonian = _get_qubit_hamiltonian_from_molecule(molecule)
 
-    # 2. Import to Symmer
+    qubit_hamiltonian = _get_qubit_hamiltonian_from_molecule(molecule)
+    number_of_qubits = count_qubits(qubit_hamiltonian)
     hamiltonian = PauliwordOp.from_openfermion(qubit_hamiltonian)
 
-    # 3. Calculate Reference Energy
-    gs_energy, _ = exact_gs_energy(hamiltonian.to_sparse_matrix)
-    nc_data["full-diagonalized_energy"] = gs_energy
-
-    # 4. Tapering
-    hartree_fock_array = np.hstack(
+    hartree_fock_state = np.hstack(
         (
             np.ones(molecule.n_electrons),
             np.zeros(molecule.n_qubits - molecule.n_electrons),
         )
     )
-    hartree_fock_array = np.array(hartree_fock_array, dtype=int)
-    tapered_hamiltonian = QubitTapering(hamiltonian).taper_it(
-        ref_state=hartree_fock_array
-    )
+    hartree_fock_state = QuantumState([np.array(hartree_fock_state, dtype=int)])
 
-    # 5. Calculate Energy Error from Tapering
+    if number_of_qubits < 20:
+        ground_state_energy, ground_state = exact_gs_energy(
+            hamiltonian.to_sparse_matrix
+        )
+        nc_data["full-diagonalized_energy"] = ground_state_energy
+        ground_state = QuantumState([ground_state.sort().state_matrix[0]])
+        nc_data["hf-fci_overlap"] = hartree_fock_state.dagger * ground_state
+
+    tapered_hamiltonian = QubitTapering(hamiltonian).taper_it(
+        ref_state=hartree_fock_state
+    )
     tapered_gs_energy, _ = exact_gs_energy(tapered_hamiltonian.to_sparse_matrix)
     nc_data["tapered-diagonalized_energy"] = tapered_gs_energy
 
-    # 6. Calculate NC Approximation
     try:
         cs_vqe = ContextualSubspace(
             tapered_hamiltonian, noncontextual_strategy=strategy
