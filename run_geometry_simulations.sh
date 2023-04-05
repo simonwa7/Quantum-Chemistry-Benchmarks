e41b9220611c7d8c9171cfb23ebafaecc4cdb0f5
simonwa7 <william.andrew.simon@gmail.com>
2023-04-05 15:31:26 -0400

    Updating cluster scripts

--- run_geometry_simulations.sh
+++ run_geometry_simulations.sh
@@ -3,8 +3,8 @@
 #SBATCH -p batch 
 #SBATCH --time=7-00:00:00
 #SBATCH -n 1
-#SBATCH -c 2
-#SBATCH --mem=36000
+#SBATCH -c 8
+#SBATCH --mem=100000
 module load anaconda/3
 source activate /cluster/tufts/lovelab/wsimon02/condaenv/qchem
 python3 run_calculations.py geometry
